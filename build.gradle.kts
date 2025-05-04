3a1fbb26cb6393d66b3a9d445fa3b5948f3c44ee
Juan Almeyda <almeydajuan@gmail.com>
2025-05-04 21:32:38 +0200

    Add docker compose for live environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.4.5")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.5")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.flywaydb:flyway-core:11.8.0")
+    implementation("org.flywaydb:flyway-database-postgresql:11.8.0")
+    implementation("org.postgresql:postgresql:42.7.5")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
     testImplementation("org.springframework.boot:spring-boot-testcontainers:3.4.5")
